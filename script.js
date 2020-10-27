b9864dc3d67e6285e1992ac7395ffa10d784fa20
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2020-10-26 23:12:38 -0400

    first draft functions made and user input variables made

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// user inputs
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//var passwordLength = prompt("How many characters in your password?(8-128)")
 
-  passwordText.value = password;
+//var passwordUppercase = confirm("Would you like uppercase letters?")
 
+//var passwordLowercase = confirm("would you like lowercase letters?")
+
+//var passwordNumbers = confirm("Would you like numbers?")
+
+//var passwordSymbols = confirm("Would you like symbols?")
+
+
+
+
+
+// generator functions
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">" , "?", "@", "[", "^", "_", "{", "|", "}", "~",];
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+console.log(getRandomLower());
+
+console.log(getRandomUpper());
+
+console.log(getRandomNumber());
+
+console.log(getRandomSymbol());
+
