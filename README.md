74d853376e5e45fe4344d15c72d556239a55e258
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2020-10-31 15:29:41 -0400

    finished work, need to add live link tom readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# password-generator-3000
+# Password Generator
+
+### Table of Contents
+
+- [Description](#description)
+- [Link to Application]()
+- [License](#license)
+
+---
+
+![Screenshot](./assets/images/passwordgenaratorprompt.png)
+![Screenshot](./assets/images/passwordgenaratorfinished.png)
+
+---
+
+
+## Description
+
+This homework was espically humbling this week but still resourceful, at first I had kept geting frustrated with my longer and more clutter filled way of arranging my arrays to use the returning boolean values with the concat() method to put them together, but by doing a little google magic I found a better way to get these values and assinging them to one string and using the setItem method we just learned to implement the string and set the generated password into the passwordText. 
+
+---
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Austyn Whaley]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+[Back To The Top](#Password-Generator)
--- script.js
+++ script.js
@@ -1,121 +1,67 @@
-// user input variables that is called when button is clicked
+//Assignment Code
+var resultEl = document.querySelector("#password");
+var generateButton = document.querySelector("#generate");
 
-function writePassword(){
-  var passwordLength = parseInt(prompt("How many characters in your password?(8-128)"))
 
 
-  if (passwordLength < 8 || passwordLength > 128){
-    alert("Length must be 8-128 characters")
+generateButton.addEventListener("click", generatePassword);
 
-  }
+function generatePassword() {
 
-  if (passwordLength >= 8 && passwordLength <= 128){
-    var passwordUppercase = confirm("Would you like uppercase letters?")
-    var passwordLowercase = confirm("Would you like lowercase letters?")
-    var passwordNumbers = confirm("Would you like numbers?")
-    var passwordSymbols = confirm("Would you like symbols?")
+    var lower = ["abcdefghijklmnopqrstuvwxyz"]
 
-    if (passwordUppercase != true && passwordLowercase != true && passwordNumbers != true && passwordSymbols != true){
-      alert("You must select at least one character type!")
-  
+    var upper = ["ABCDEFGHIJKLMNOPQRSTUZWXYZ"]
+
+    var numbers = ["0123456789"]
+
+    var symbols = ["!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~"]
+
+    var generatePassword = "";
+    var passwordOptions = "";
+
+    var passwordLength = prompt("How many characters would you like your password to be?");
+
+    if (Number(passwordLength < 8) | Number(passwordLength > 128) | isNaN(passwordLength)) {
+        alert("Please enter a number between 8 and 128!");
+        return;
+    } 
+    //pick characters from arrays
+    var lowercaseCharacters = confirm("Would you like lowercase letters?");
+
+    if (lowercaseCharacters) {
+        passwordOptions += lower;
     }
 
-  }
+    var uppercaseCharacters = confirm("Would you like uppercase letters?");
 
-  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
+    if (uppercaseCharacters) {
+        passwordOptions += upper;
+    }
 
-  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
+    var numberCharacters = confirm("Would you like numbers?");
 
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    if (numberCharacters) {
+        passwordOptions += numbers;
+    }
 
-  var symbols = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">" , "?", "@", "[", "^", "_", "{", "|", "}", "~",];
+    var specialCharactersInput = confirm("Would you like symbols?");
 
-  var possibleCharacter = [];
+    if (specialCharactersInput) {
+        passwordOptions += symbols;
+    }
 
-  var generatedPassword = "";
-
-  //build list of possible values
-  if (passwordNumbers && passwordSymbols && passwordUppercase && passwordLowercase){
-     possibleCharacter = possibleCharacter.concat(numbers, symbols, upper, lower) 
-  }
-
-  else if (passwordSymbols && passwordUppercase && passwordLowercase){
-    possibleCharacter = possibleCharacter.concat(symbols, upper, lower)
-  }
-
-  else if (passwordSymbols && passwordNumbers && passwordLowercase){
-    possibleCharacter = possibleCharacter.concat(symbols, numbers, lower)
-  }
-  else if (passwordSymbols && passwordUppercase && passwordLowercase){
-    possibleCharacter = possibleCharacter.concat(symbols, numbers, upper)
-  }
-  else if (passwordSymbols && passwordUppercase && passwordLowercase){
-    possibleCharacter = possibleCharacter.concat(numbers, upper, lower)
-  }
-
-  else if (passwordUppercase && passwordNumbers){
-    possibleCharacter = possibleCharacter.concat(upper, numbers)
-  }
-
-  else if (passwordUppercase && passwordLowercase){
-    possibleCharacter = possibleCharacter.concat(upper, lower)
-  }
-
-  else if (passwordUppercase && passwordSymbols){
-    possibleCharacter = possibleCharacter.concat(upper, symbols)
-  }
-
-  else if (passwordNumbers && passwordSymbols){
-    possibleCharacter = possibleCharacter.concat(numbers,symbols)
-  }
-  else if (passwordNumbers && passwordLowercase){
-    possibleCharacter = possibleCharacter.concat(numbers,lower)
-  }
-
-  else if (passwordLowercase && passwordSymbols){
-    possibleCharacter = possibleCharacter.concat(lower, symbols)
-  }
-
-  else if (passwordLowercase){
-    possibleCharacter = possibleCharacter.concat(lower)
-  }
-
-  else if (passwordUppercase){
-    possibleCharacter = possibleCharacter.concat(upper)
-  }
-
-  else if (passwordNumbers){
-    possibleCharacter = possibleCharacter.concat(numbers)
-  }
-
-  else if (passwordSymbols){
-    possibleCharacter = possibleCharacter.concat(symbols)
-  }
-
-  //loop over length of password
-  for (var i = 0; i < passwordLength; i++){
-    //pick random character from array
-    generatedPassword += possibleCharacter[Math.floor(Math.random) * possibleCharacter.length];
+    if (passwordOptions === "") {
+        alert("You must select at least one character type!");
+    }
     
-    
-  }
-  //return password add character to password string
-  return generatedPassword;
 
-}
-
-
-//Assignment Code
-var resultEl = document.querySelector("#result");
-var generateEl = document.querySelector("#generate");
-
-// Add event listener to generate button
-generateEl.addEventListener("click", writePassword);
-
-
-
-
-// make an array of characters for uppercase, lowercase, and special characters to use for the generator
-// once user clicks,then prompt the user for specific criteria and use that criteria fpr the generator
-// prompt them for the length of password between 8-128 characters 
-// once all prompts are complete then display the results in an alert or on the page
+    //loop over length of password
+    if (Number(passwordLength >= 8) && Number(passwordLength <= 128)) {
+        for (var i = 0; i < passwordLength; i++) {
+            generatePassword += passwordOptions.charAt(Math.floor(Math.random() * passwordOptions.length));
+        };
+        localStorage.setItem("password", generatePassword);
+        var generatedPasswordStr = localStorage.getItem("password");
+        resultEl.textContent = generatedPasswordStr;
+    };
+};
