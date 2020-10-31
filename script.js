ce1a10c3ff45af8dff700072ce72122f9a419906
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2020-10-30 21:37:00 -0400

    updated script, index, and style files

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// user input variables
+// user input variables that is called when button is clicked
 
-function generatePassword(){
-  var passwordLength = prompt("How many characters in your password?(8-128)")
+function writePassword(){
+  var passwordLength = parseInt(prompt("How many characters in your password?(8-128)"))
+
 
   if (passwordLength < 8 || passwordLength > 128){
     alert("Length must be 8-128 characters")
-    
-    generatePassword()
+
   }
 
   if (passwordLength >= 8 && passwordLength <= 128){
@@ -14,64 +14,103 @@ function generatePassword(){
     var passwordLowercase = confirm("Would you like lowercase letters?")
     var passwordNumbers = confirm("Would you like numbers?")
     var passwordSymbols = confirm("Would you like symbols?")
-  }
 
-  if (passwordUppercase != true && passwordLowercase != true && passwordNumbers != true && passwordSymbols != true){
-    alert("You must select at least one character type!")
-  }
-}
-
-//DOM element
-
-var resultEl = document.getElementById("result");
-
-
-
-var randomFunction = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
-
-function getRandomLower() {
-  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"]
-  return lower[Math.floor(Math.random() * lower.length)];
-}
-
-function getRandomUpper() {
- var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"]
-  return upper[Math.floor(Math.random() * upper.length)];
-}
-
-function getRandomNumber() {
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function getRandomSymbol() {
-  var symbols = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">" , "?", "@", "[", "^", "_", "{", "|", "}", "~",];
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-
-
-
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+    if (passwordUppercase != true && passwordLowercase != true && passwordNumbers != true && passwordSymbols != true){
+      alert("You must select at least one character type!")
   
-//   var generatedPassword
-//   passwordText.value = password;
+    }
 
-// }
+  }
+
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
+
+  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
+
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  var symbols = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">" , "?", "@", "[", "^", "_", "{", "|", "}", "~",];
+
+  var possibleCharacter = [];
+
+  var generatedPassword = "";
+
+  //build list of possible values
+  if (passwordNumbers && passwordSymbols && passwordUppercase && passwordLowercase){
+     possibleCharacter = possibleCharacter.concat(numbers, symbols, upper, lower) 
+  }
+
+  else if (passwordSymbols && passwordUppercase && passwordLowercase){
+    possibleCharacter = possibleCharacter.concat(symbols, upper, lower)
+  }
+
+  else if (passwordSymbols && passwordNumbers && passwordLowercase){
+    possibleCharacter = possibleCharacter.concat(symbols, numbers, lower)
+  }
+  else if (passwordSymbols && passwordUppercase && passwordLowercase){
+    possibleCharacter = possibleCharacter.concat(symbols, numbers, upper)
+  }
+  else if (passwordSymbols && passwordUppercase && passwordLowercase){
+    possibleCharacter = possibleCharacter.concat(numbers, upper, lower)
+  }
+
+  else if (passwordUppercase && passwordNumbers){
+    possibleCharacter = possibleCharacter.concat(upper, numbers)
+  }
+
+  else if (passwordUppercase && passwordLowercase){
+    possibleCharacter = possibleCharacter.concat(upper, lower)
+  }
+
+  else if (passwordUppercase && passwordSymbols){
+    possibleCharacter = possibleCharacter.concat(upper, symbols)
+  }
+
+  else if (passwordNumbers && passwordSymbols){
+    possibleCharacter = possibleCharacter.concat(numbers,symbols)
+  }
+  else if (passwordNumbers && passwordLowercase){
+    possibleCharacter = possibleCharacter.concat(numbers,lower)
+  }
+
+  else if (passwordLowercase && passwordSymbols){
+    possibleCharacter = possibleCharacter.concat(lower, symbols)
+  }
+
+  else if (passwordLowercase){
+    possibleCharacter = possibleCharacter.concat(lower)
+  }
+
+  else if (passwordUppercase){
+    possibleCharacter = possibleCharacter.concat(upper)
+  }
+
+  else if (passwordNumbers){
+    possibleCharacter = possibleCharacter.concat(numbers)
+  }
+
+  else if (passwordSymbols){
+    possibleCharacter = possibleCharacter.concat(symbols)
+  }
+
+  //loop over length of password
+  for (var i = 0; i < passwordLength; i++){
+    //pick random character from array
+    generatedPassword += possibleCharacter[Math.floor(Math.random) * possibleCharacter.length];
+    
+    
+  }
+  //return password add character to password string
+  return generatedPassword;
+
+}
+
+
+//Assignment Code
+var resultEl = document.querySelector("#result");
+var generateEl = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateEl.addEventListener("click", writePassword);
 
 
 
@@ -79,4 +118,4 @@ generateBtn.addEventListener("click", generatePassword);
 // make an array of characters for uppercase, lowercase, and special characters to use for the generator
 // once user clicks,then prompt the user for specific criteria and use that criteria fpr the generator
 // prompt them for the length of password between 8-128 characters 
-// once all prompts are complete then display the results in an alert of on the page
+// once all prompts are complete then display the results in an alert or on the page
